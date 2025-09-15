b3a4c93a4da59fcb3fc8dbcc44c10830e751c8ac
lucas-sv <144680462+waytix-l@users.noreply.github.com>
2025-09-15 23:35:34 +0200

    aaaapp

--- next.config.js
+++ next.config.js
@@ -1,9 +1,30 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  basePath: '/waytix-l.github.io',
-   assetPrefix: '/waytix-l.github.io/'
-};
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  // Remplacez 'portfolio-lucas-savy' par le nom exact de votre repository
+  basePath: isProd ? '/waytix-l.github.io' : '',
+  assetPrefix: isProd ? '/waytix-l.github.io/' : '',
+  
+  // Configuration importante pour les assets CSS
+  experimental: {
+    optimizeCss: false,
+  },
+  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  
+  // Configuration pour les assets statiques
+  env: {
+    CUSTOM_KEY: 'my-value',
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig

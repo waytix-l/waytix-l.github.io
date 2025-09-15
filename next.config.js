c520add6eb3f6ff189d907813a6b0e8b60f51ef6
lucas-sv <144680462+waytix-l@users.noreply.github.com>
2025-09-15 23:16:58 +0200

    aaab

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/waytix-l.github.io'
+  basePath: '/waytix-l.github.io',
+   assetPrefix: '/waytix-l.github.io/'
 };
 
 module.exports = nextConfig;

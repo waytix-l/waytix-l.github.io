82616ed7fe20c2a0b0ec969cfe2d460713a2af07
lucas-sv <144680462+waytix-l@users.noreply.github.com>
2025-09-15 23:05:08 +0200

    aaaaa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  distDir: 'out',
-  images: {
-    unoptimized: true
-  },
-  // Remplacez 'votre-username' et 'nom-du-repo' par vos vraies valeurs
-  basePath: process.env.NODE_ENV === 'production' ? '/waytix-l.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/waytix-l.github.io/' : '',
-}
+  images: { unoptimized: true },
+  basePath: '/waytix-l.github.io'
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
